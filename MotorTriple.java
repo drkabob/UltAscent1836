70187b7960d03ec0978d1b575eda421406835d34
Daniel Kessler <sinani201@gmail.com>
2013-02-17 14:49:57 -0800

    added implementation for MotorTriple

--- MotorTriple.java
+++ MotorTriple.java
@@ -6,35 +6,70 @@ public class MotorTriple implements SpeedController {
 	private SpeedController ma;
 	private SpeedController mb;
 	private SpeedController mc;
+
+	private boolean reva;
+	private boolean revb;
+	private boolean revc;
+
+	private double speed;
 	
 	public MotorTriple(SpeedController a, SpeedController b, SpeedController c) {
-		
+		ma = a;
+				
 	}
+
+	public MotorTriple(SpeedController a, boolean arev, SpeedController b, boolean brev, SpeedController c, boolean crev) {
+		ma = a;
+		ma.set(0);
+		reva = arev;
+
+		mb = b;
+		mb.set(0);
+		revb = brev;
+		
+		mc = c;
+		mc.set(0);
+		revc = crev;
+	}
+
 	@Override
 	public void disable() {
-		
+		ma.disable();
+		mb.disable();
+		mc.disable();
 	}
 
 	@Override
 	public double get() {
-		// TODO Auto-generated method stub
-		return 0;
+		return speed;
+	}
+
+	public double getA() {
+		return ma.get();
+	}
+	public double getB() {
+		return mb.get();
+	}
+	public double getC() {
+		return mc.get();
 	}
 
 	@Override
-	public void set(double arg0) {
-		// TODO Auto-generated method stub
-
+	public void set(double newspeed) {
+		ma.set(reva ? -newspeed : newspeed);
+		mb.set(revb ? -newspeed : newspeed);
+		mc.set(revc ? -newspeed : newspeed);
 	}
 
 	@Override
-	public void set(double arg0, byte arg1) {
-		// TODO Auto-generated method stub
-
+	public void set(double newspeed, byte arg1) {
+		set(newspeed);
 	}
 	@Override
-	public void pidWrite(double arg0) {
-
+	public void pidWrite(double output) {
+		ma.pidWrite(reva ? -output : output);
+		mb.pidWrite(revb ? -output : output);
+		mc.pidWrite(revc ? -output : output);
 	}
 
 	
