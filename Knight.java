8c555f55dd326b71b4bdf798eb63524b24d7eb89
Daniel Kessler <sinani201@gmail.com>
2013-03-22 13:55:40 -0700

    made the delay in autonomous longer

--- Knight.java
+++ Knight.java
@@ -146,7 +146,7 @@ public class Knight extends IterativeRobot {
 	double last_timer;
 	boolean frisbeeDone;
 	final double WAIT_AFTER_ACTUATOR = 5;
-	final double DELAY_BETWEEN_FRISBEES = 1;
+	final double DELAY_BETWEEN_FRISBEES = 2.25;
 	final double FRISBEE_SHOOT_TIME = 0.25;
 
 	final double DRIVE_FORWARD_TIME = 2;
