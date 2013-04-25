b4a9f923dae7f8a17cd5c7b6f23961997f03cdf8
Daniel Kessler <sinani201@gmail.com>
2013-04-25 06:05:37 -0700

    added Pulse.java which mysteriously disappeared

---
+++ Pulse.java
@@ -0,0 +1,40 @@
+package com.milkenknights;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Timer;
+
+// this implementation is BAD!
+// TODO: use threads
+// TODO: extend SpeedController
+public class Pulse {
+	private Talon t;
+	private double pulseTime, delay;
+	private double lastShootTime;
+	private boolean isShooting;
+
+	public Pulse(int channel, double pulseTime, double delay) {
+		t = new Talon(channel);
+		this.pulseTime = pulseTime;
+		this.delay = delay;
+	}
+
+	public void set(double speed) {
+		if (speed == 0) {
+			isShooting = false;
+			t.set(0);
+		} else {
+			if (isShooting) {
+				if (Timer.getFPGATimestamp() - lastShootTime >= pulseTime + delay) {
+					lastShootTime = Timer.getFPGATimestamp();
+				} else if (Timer.getFPGATimestamp() - lastShootTime > pulseTime) {
+					t.set(0);
+				} else {
+					t.set(speed);
+				}
+			} else {
+				isShooting = true;
+				lastShootTime = Timer.getFPGATimestamp();
+			}
+		}
+	}
+}
